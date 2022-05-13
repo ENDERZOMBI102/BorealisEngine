6bd0cb9d7940210b6c2ed7a6a56c9981d84db732
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-05-13 12:26:28 +0200

    move layers to their own file

--- filesystem/src/layered/mod.rs
+++ filesystem/src/layered/mod.rs
@@ -1,9 +1,11 @@
+pub mod layers;
+
 use std::fs::File;
 use std::io::{ErrorKind, Read};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
-use path_slash::PathBufExt;
-use crate::upkf::{Element, Upkf};
+use crate::upkf::Element;
+use crate::layered::layers::{FolderLayer, UpkfLayer};
 
 pub enum LayeredFile<'a> {
 	Rust { file: File },
@@ -90,103 +92,11 @@ impl LayeredFS {
 
 }
 
-struct FolderLayer {
-	path: PathBuf
-}
-impl FolderLayer {
-	fn from_path( folder: &Path ) -> Self {
-		FolderLayer {
-			path: folder.clone().to_path_buf()
-		}
-	}
-
-	fn from_buf( folder: PathBuf ) -> Self {
-		FolderLayer {
-			path: folder.clone()
-		}
-	}
-}
-
-impl Layer for FolderLayer {
-	fn resolve( &self, filename: &str ) -> PathBuf {
-		let mut path = self.path.clone();
-		path.push( filename );
-		path
-	}
-	
-	fn contains( &self, filename: &str ) -> bool {
-		self.resolve( filename ).exists()
-	}
-
-	fn get_file( &self, filename: &str ) -> Result<LayeredFile, ErrorKind> {
-		let file = File::open( self.resolve( filename ) );
-		if file.is_ok() {
-			return Ok( LayeredFile::Rust { file: file.unwrap() } )
-		}
-		Err( ErrorKind::Other )
-	}
-
-	fn meta( &self ) -> LayerMeta {
-		LayerMeta {
-			origin: None,
-			filename: self.path.to_str().unwrap().to_string(),
-			size: None
-		}
-	}
-}
-
-struct UpkfLayer {
-	upkf: Upkf
-}
-
-impl UpkfLayer {
-	fn from_buf( path: PathBuf ) -> Self {
-		UpkfLayer {
-			upkf: Upkf::load( path.as_path(), true )
-		}
-	}
-}
-
-impl Layer for UpkfLayer {
-	fn resolve( &self, filename: &str ) -> PathBuf {
-		let mut path = PathBuf::from( String::from( self.upkf.get_path().unwrap().to_str().unwrap() ) + "!" );
-		path.push( filename );
-		path.to_slash().unwrap().parse().unwrap()
-	}
-
-	fn contains( &self, filename: &str ) -> bool {
-		for file in self.upkf.iter() {
-			if file.get_path() == filename {
-				return true
-			}
-		}
-		false
-	}
-
-	fn get_file( &self, filename: &str ) -> Result<LayeredFile, ErrorKind> {
-		for element in self.upkf.iter() {
-			if element.get_path() == filename {
-				return Ok( LayeredFile::Upkf { element: Arc::new( &element ) } )
-			}
-		}
-		Err( ErrorKind::NotFound )
-	}
-
-	fn meta( &self ) -> LayerMeta {
-		LayerMeta {
-			origin: Some( self.upkf.get_origin().to_string() ),
-			filename: self.upkf.get_path().unwrap().to_str().unwrap().to_string(),
-			size: Some( File::open( self.upkf.get_path().unwrap() ).unwrap().metadata().unwrap().len() )
-		}
-	}
-}
-
-
 pub fn main() {
 	let mut fs = LayeredFS { layers: vec![
-		Box::new( FolderLayer { path: Path::new(".").canonicalize().unwrap().to_path_buf() } ),
-		Box::new( FolderLayer { path: Path::new("./../tast").canonicalize().unwrap().to_path_buf() } ),
-		// Box::new( UpkfLayer { upkf: Upkf::load( Path::new("./cfg.upkf"), true ) } )
+		Box::new( FolderLayer::from_buf( Path::new(".").canonicalize().unwrap() ) ),
+		Box::new( FolderLayer::from_buf( Path::new("./../tast").canonicalize().unwrap() ) ),
+		// Box::new( UpkfLayer::from_buf( Upkf::load( Path::new("./cfg.upkf"), true ) ) )
 	] };
 
 	let mut input = String::new();
