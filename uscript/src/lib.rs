cb51724b50af35c4e40ebc41f7f8b81ed94666b8
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2023-04-17 17:08:16 +0200

    Testing generics
    ~ ender

--- uscript/src/lib.rs
+++ uscript/src/lib.rs
@@ -33,20 +33,15 @@ pub trait RegisterNativeFunction< A: 'static, const N: usize, const X: bool, R: 
 fn testing() {
 	fn hello() { println!("h") }
 
-	pub fn register_fn< A: 'static, const N: usize, const C: bool, R: Variant + Clone, const L: bool, F: RegisterNativeFunction<A, N, C, R, L> >( name: impl AsRef<str>, func: F, ) {
+	pub fn register_fn<
+		A: 'static,
+		const N: usize,
+		const C: bool,
+		R: Variant + Clone,
+		const L: bool,
+		F: RegisterNativeFunction<A, N, C, R, L>
+	>( name: impl AsRef<str>, func: F, ) {
 		let param_types = F::param_types();
-
-		let mut param_type_names: crate::StaticVec<_> = F::param_names()
-			.iter()
-			.map( |ty| format!( "_: {}", self.format_type_name( ty ) ) )
-			.collect();
-
-		if F::return_type() != TypeId::of::<()>() {
-			param_type_names.push( self.format_type_name( F::return_type_name() ).into() );
-		}
-
-		let param_type_names: crate::StaticVec<_> = param_type_names.iter().map( String::as_str ).collect();
-		let param_type_names = Some( param_type_names.as_ref() );
 
 		let fn_name = name.as_ref();
 
@@ -56,7 +51,7 @@ fn testing() {
 			F::num_params() == 2 && fn_name.starts_with( "set$" )
 		);
 
-		let func = func.into_callable_function(fn_name.into(), no_const);
+
 
 
 	}
